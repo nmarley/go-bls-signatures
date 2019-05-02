5f6b3d34eeb3e8e00962de68dd1152ac90fe500c
Nathan Marley <nathan.marley@gmail.com>
2019-05-01 23:20:10 -0300

    Add Hash512 to hash funcs

--- hash_funcs.go
+++ hash_funcs.go
@@ -12,6 +12,14 @@ func Hash256(m []byte) []byte {
 	return h.Sum(nil)
 }
 
+// Hash512 function with 512 bit outputs.
+func Hash512(m []byte) []byte {
+	h1 := Hash256(append(m, 0x00))
+	h2 := Hash256(append(m, 0x01))
+
+	return append(h1, h2...)
+}
+
 // Hmac256 returns a HMAC using SHA256.
 func Hmac256(m, key []byte) []byte {
 	// Create a new HMAC by defining the hash type and the key (as byte array)
