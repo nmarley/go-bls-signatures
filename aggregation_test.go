cd10953d6409609cc648ecaaab28cc55995452d7
Nathan Marley <nathan.marley@gmail.com>
2019-05-14 10:59:42 -0300

    WIP: start on sig aggregatino

---
+++ aggregation_test.go
@@ -0,0 +1,35 @@
+package bls_test
+
+import (
+	"fmt"
+	// "math/big"
+	"github.com/matryer/is"
+	"gitlab.com/nmarley/go-bls-signatures"
+	"testing"
+)
+
+func TestVectorAggregation(t *testing.T) {
+	tests := []struct {
+		sigs [][]byte
+	}{
+		{
+			sigs: [][]byte{sig1, sig2},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(st *testing.T) {
+			is := is.New(st)
+			var sigs []*bls.Signature
+			for _, sigBytes := range tt.sigs {
+				signature, _ := bls.DeserializeSignature(sigBytes)
+				sigs = append(sigs, signature)
+			}
+			fmt.Println("sigs:", sigs)
+			// bls.AggregateSignatures
+			// Aggregate(sig1, sig2)
+			is.Equal(1, 7)
+		})
+	}
+
+}
