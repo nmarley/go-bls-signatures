df6e0d42df63452441f2768d367c06ff94f7eed2
Nathan Marley <nathan.marley@gmail.com>
2019-05-17 00:10:20 -0300

    cComment debug in test

--- aggregation_test.go
+++ aggregation_test.go
@@ -107,30 +107,43 @@ func TestVectorAggregation(t *testing.T) {
 func TestVectorAggregation2(t *testing.T) {
 	is := is.New(t)
 
-	// TODO: Rename variables... sk1 => sk1Bytes, sk1o => sk1
-	sk1 := bls.DeserializeSecretKey(sk1Bytes)
-	sk2 := bls.DeserializeSecretKey(sk2Bytes)
+	m1 := []byte{1, 2, 3, 40}
+	//m2 := []byte{5, 6, 70, 201}
+	m3 := []byte{9, 10, 11, 12, 13}
+	//m4 := []byte{15, 63, 244, 92, 0, 1}
 
-	sig1 := sk1.Sign([]byte{1, 2, 3, 40})
-	sig2 := sk2.Sign([]byte{5, 6, 70, 201})
-	sig3 := sk2.Sign([]byte{1, 2, 3, 40})
-	sig4 := sk1.Sign([]byte{9, 10, 11, 12, 13})
-	sig5 := sk1.Sign([]byte{1, 2, 3, 40})
-	sig6 := sk1.Sign([]byte{15, 63, 244, 92, 0, 1})
+	sk1 := bls.SecretKeyFromSeed([]byte{1, 2, 3, 4, 5})
+	sk2 := bls.SecretKeyFromSeed([]byte{1, 2, 3, 4, 5, 6})
 
-	sigL := bls.AggregateSignatures([]*bls.Signature{sig1, sig2})
+	//sig1 := sk1.Sign(m1)
+	//sig2 := sk2.Sign(m2)
+	sig3 := sk2.Sign(m1)
+	sig4 := sk1.Sign(m3)
+	sig5 := sk1.Sign(m1)
+	//sig6 := sk1.Sign(m4)
+
+	//fmt.Printf("NGMgo(tv2) sig1: %x\n", sig1.Serialize())
+	//fmt.Printf("NGMgo(tv2) sig2: %x\n", sig2.Serialize())
+	//fmt.Printf("NGMgo(tv2) sig3: %x\n", sig3.Serialize())
+	//fmt.Printf("NGMgo(tv2) sig4: %x\n", sig4.Serialize())
+	//fmt.Printf("NGMgo(tv2) sig5: %x\n", sig5.Serialize())
+	//fmt.Printf("NGMgo(tv2) sig6: %x\n", sig6.Serialize())
+
+	//sigL := bls.AggregateSignatures([]*bls.Signature{sig1, sig2})
+	//fmt.Printf("NGMgo(tv2) sigL: %x\n", sigL.Serialize())
 	sigR := bls.AggregateSignatures([]*bls.Signature{sig3, sig4, sig5})
+	//fmt.Printf("NGMgo(tv2) sigR: %x\n", sigR.Serialize())
 
-	is.True(sigL.Verify())
+	//is.True(sigL.Verify())
 	//is.True(sigR.Verify())
 
-	sigFinal := bls.AggregateSignatures([]*bls.Signature{sigL, sigR, sig6})
-
-	sigFinal.Serialize()
-	fmt.Printf("NGMgo - sigFinal: %096x\n", sigFinal.Serialize())
+	//sigFinal := bls.AggregateSignatures([]*bls.Signature{sigL, sigR, sig6})
+	//
+	//sigFinal.Serialize()
+	//fmt.Printf("NGMgo - sigFinal: %096x\n", sigFinal.Serialize())
 	//is.Equal(fmt.Sprintf("%096x", sigFinal.Serialize()), "8b11daf73cd05f2fe27809b74a7b4c65b1bb79cc1066bdf839d96b97e073c1a635d2ec048e0801b4a208118fdbbb63a516bab8755cc8d850862eeaa099540cd83621ff9db97b4ada857ef54c50715486217bd2ecb4517e05ab49380c041e159b")
 
-	is.True(sigFinal.Verify())
+	//is.True(sigFinal.Verify())
 
 	// 0x07969958fbf82e65bd13ba0749990764cac81cf10d923af9fdd2723f1e3910c3fdb874a67f9d511bb7e4920f8c01232b12e2fb5e64a7c2d177a475dab5c3729ca1f580301ccdef809c57a8846890265d195b694fa414a2a3aa55c32837fddd80
 }
